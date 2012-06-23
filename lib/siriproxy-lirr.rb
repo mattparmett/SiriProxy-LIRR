fc54aaa9d869b30f9888cf7d00511e499f188579
parm289 <parm289@yahoo.com>
2012-06-22 20:14:17 -0400

    make siri plugin

---
+++ lib/siriproxy-lirr.rb
@@ -0,0 +1,23 @@
+#Useful for siri query "When's the next train from ___ to ___?"
+require 'rubygems'
+require './lirr.rb'
+
+class SiriProxy::Plugin::LIRR < SiriProxy::Plugin
+	def initialize(config)
+	end
+
+
+	listen_for /when is the next train from (.*) to (.*)?/i do |from_station_name, to_station_name|
+		from_station = Station.new(from_station_name)
+		to_station = Station.new(to_station_name)
+		train = getNextTrain(from_station, to_station)
+
+		if train.has_transfer?
+			say "The next train from " + train.from_station_name + " to " + train.to_station_name + " leaves at " + train.dep_time + " and arrives at " + train.arr_time + ", with a transfer at " + train.trans_station_name + " at " + train.trans_time + "."
+
+		else
+			say "The next train from " + train.from_station_name + " to " + train.to_station_name + " leaves at " + train.dep_time + " and arrives at " + train.arr_time + "."
+		end
+
+	end
+end
