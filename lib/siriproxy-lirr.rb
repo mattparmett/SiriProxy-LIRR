db9b0ee95326f5bc33779dbb60ea81d54ba0a0db
parm289 <parm289@yahoo.com>
2012-06-24 01:06:51 -0400

    clean

--- lib/siriproxy-lirr.rb
+++ lib/siriproxy-lirr.rb
@@ -62,7 +62,29 @@ class SiriProxy::Plugin::LIRR < SiriProxy::Plugin
 			end
 			say "Here are the train times for " + from_station.name + " to " + to_station.name + ":\n\n" + train_times, spoken: "Here are the train times for " + from_station.name + " to " + to_station.name + "."
 
-#+ from_station.name + " to " + to_station.name + ":\n\n" + trains[0].to_timetable + "\n" + trains[1].to_timetable + "\n" + trains[2].to_timetable + "\n" + trains[3].to_timetable + "\n" + trains[4].to_timetable + "\n", spoken: "Here are the train times for " + from_station.name + " to " + to_station.name + "."
+		end
+	end
+
+	def trainSearch(from_station_name, to_station_name, time, am_pm, date)
+		from_station = Station.new(from_station_name, self.stations_csv_file)
+		to_station = Station.new(to_station_name, self.stations_csv_file)
+		
+		trains = getTrainTimes(from_station, to_station, time, am_pm, date, self.stations_csv_file)
+
+		if trains == []
+			say "Error: no trains found."
+		else
+			i = 0
+			train_times = ""
+			while i < 4 #i = 0-3
+				train_times << trains[i].to_timetable + "\n"
+				i = i + 1
+			end
+		
+			train_times << trains[i].to_timetable #i = 4
+
+			say "Here are the train times for " + from_station.name + " to " + to_station.name + " around " + time + ":\n\n" + train_times, spoken: "Here are the train times for " + from_station.name + " to " + to_station.name + "."
+
 		end
 	end
 
@@ -93,5 +115,25 @@ class SiriProxy::Plugin::LIRR < SiriProxy::Plugin
 		to_station_name = to_station_name.gsub(/\w+/) {|word|  word.capitalize}	
 		trainSchedule(from_station_name, to_station_name)
 		request_completed	
+	end
+
+	listen_for /search for trains from ([a-z ]*) to ([a-z ]*) at ([0-9,]*[0-9]):([0-9,]*[0-9]) ([a-z]*)/i do |from_station_name, to_station_name, hour, minutes, am_pm|
+		from_station_name = from_station_name.gsub(/\w+/) {|word|  word.capitalize}
+		to_station_name = to_station_name.gsub(/\w+/) {|word|  word.capitalize}
+		
+		#Convert hour to proper format
+    		hour = hour.to_i
+   		 if (hour < 10)
+   		 	hour = hour.to_s()
+   		 	hour = '0' + hour
+		 else
+  		  	hour = hour.to_s()
+		end
+
+		#Convert AM/PM to proper format
+		am_pm = am_pm.upcase
+
+		trainSearch(from_station_name, to_station_name, time, am_pm, getTodaysDate())
+		request_completed
 	end
 end
